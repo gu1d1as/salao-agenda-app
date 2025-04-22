c8a5824ce69a3b1102780432a551d64f8c404b62
gu1d1as <127784506+gu1d1as@users.noreply.github.com>
2025-04-22 11:16:08 -0300

    Update MainActivity.kt

--- salao-agenda-app/app/src/main/java/com/seuprojeto/salaoagenda/MainActivity.kt
+++ salao-agenda-app/app/src/main/java/com/seuprojeto/salaoagenda/MainActivity.kt
@@ -1,12 +1,32 @@
-package com.seuprojeto.salaoagenda
+package com.seuapp.agendamentolapa
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.seuprojeto.salaoagenda.R
 
 class MainActivity : AppCompatActivity() {
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nomeInput = findViewById<EditText>(R.id.inputNome)
+        val dataInput = findViewById<EditText>(R.id.inputData)
+        val horaInput = findViewById<EditText>(R.id.inputHora)
+        val btnAgendar = findViewById<Button>(R.id.btnAgendar)
+
+        btnAgendar.setOnClickListener {
+            val nome = nomeInput.text.toString()
+            val data = dataInput.text.toString()
+            val hora = horaInput.text.toString()
+
+            if (nome.isNotEmpty() && data.isNotEmpty() && hora.isNotEmpty()) {
+                Toast.makeText(this, "Agendamento feito para $nome em $data às $hora", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
